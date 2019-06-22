add5a26df98bec1faf6670f8e0cdaf3437738a1d
Artyom Mironov <kazufukurou@gmail.com>
2019-06-22 20:24:40 +0900

    update configs

--- .local/bin/swaystatus.py
+++ .local/bin/swaystatus.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import json
 import i3ipc
 import threading
 import subprocess
@@ -27,7 +28,7 @@ def on_window_focus(ipc, focused):
 
 def print_status(text, highlight):
     color = color1 if highlight else color2
-    print('{"full_text":"%s","color":"#%s"},' %(text, color), flush = True)
+    print('{"full_text":%s,"color":"#%s"},' %(json.dumps(text), color), flush = True)
 
 ipc.on("window::focus", on_window_focus)
 t = threading.Thread(target=ipc.main)
@@ -41,9 +42,11 @@ while True:
     volume_state = amixer.decode('utf-8').split()[-1].strip('[]')
     print('[')
     print_status(prev_focused.name, False)
-    print_status('  V', False)
+    print_status(' ', False)
+    print_status('V', False)
     print_status(volume_level, volume_state == 'on')
-    print_status(time.strftime('  %b'), False)
+    print_status(' ', False)
+    print_status(time.strftime('%b'), False)
     print_status(time.strftime('%d'), True)
     print_status(time.strftime('%a'), False)
     print_status(time.strftime('%R'), True)
